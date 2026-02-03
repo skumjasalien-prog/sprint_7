423d5a11adfe6d70c7ce32d4bb0eeca866e0091f
svetlana <skumjasalien@gmail.com>
2026-02-03 12:13:51 +0300

    исправила ошибки

--- src/test/java/api/tests/CourierCreationTest.java
+++ src/test/java/api/tests/CourierCreationTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.equalTo;
 
 public class CourierCreationTest extends BaseTest {
-
     private final CourierApi courierApi = new CourierApi();
     private Integer courierId;
 
@@ -33,5 +32,43 @@ public class CourierCreationTest extends BaseTest {
 
         Response login = courierApi.login(new LoginRequest("liz_test", "1234"));
         courierId = login.then().extract().path("id");
+    }
+
+    @Test
+    public void cannotCreateTwoIdenticalCouriers() {
+        Courier courier = new Courier("duplicate_test", "1234", "Liz");
+
+        courierApi.createCourier(courier)
+                .then()
+                .statusCode(201)
+                .body("ok", equalTo(true));
+
+        Response login = courierApi.login(new LoginRequest("duplicate_test", "1234"));
+        courierId = login.then().extract().path("id");
+
+        courierApi.createCourier(courier)
+                .then()
+                .statusCode(409)
+                .body("message", equalTo("Этот логин уже используется"));
+    }
+
+    @Test
+    public void cannotCreateCourierWithoutLogin() {
+        Courier courier = new Courier(null, "1234", "Liz");
+
+        courierApi.createCourier(courier)
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
+    }
+
+    @Test
+    public void cannotCreateCourierWithoutPassword() {
+        Courier courier = new Courier("no_password_test", null, "Liz");
+
+        courierApi.createCourier(courier)
+                .then()
+                .statusCode(400)
+                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
     }
 }
